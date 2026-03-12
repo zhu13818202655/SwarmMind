b0a194e2c866dfc5ef3fee52d13e7b69245d52da
Louis <18810151125@163.com>
2026-03-12 21:08:38 +0800

    update

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
-import requests
+import os
+import asyncio
 
-def google_search(query, api_key, cx, num=10):
-    url = "https://www.googleapis.com/customsearch/v1"
-    params = {
-        'q': query,
-        'key': api_key,
-        'cx': cx,
-        'num': num
-    }
-    response = requests.get(url, params=params)
-    if response.status_code == 200:
-        data = response.json()
-        items = data.get('items', [])
-        for item in items:
-            print(f"标题: {item['title']}")
-            print(f"链接: {item['link']}")
-            print(f"摘要: {item.get('snippet')}\n")
-    else:
-        print(f"请求失败，状态码: {response.status_code}")
-        print(response.text)
+from agentscope.message import Msg
+from agentscope.memory import InMemoryMemory, Mem0LongTermMemory
+from agentscope.agent import ReActAgent
+from agentscope.embedding import DashScopeTextEmbedding
+from agentscope.formatter import DashScopeChatFormatter
+from agentscope.model import DashScopeChatModel
+from agentscope.tool import Toolkit
 
-# 使用示例
-API_KEY = "7723daaff2a2155b7882d8d8cee6fbb444e07d8c"
-CX = "114347988173001436116"
-google_search("Python教程", API_KEY, CX)
+
+# 创建 mem0 长期记忆实例
+long_term_memory = Mem0LongTermMemory(
+    agent_name="Friday",
+    user_name="user_123",
+    model=DashScopeChatModel(
+        model_name="qwen-max-latest",
+        api_key=os.environ.get("DASHSCOPE_API_KEY"), # pyright: ignore[reportArgumentType]
+        stream=False,
+    ),
+    embedding_model=DashScopeTextEmbedding(
+        model_name="text-embedding-v2",
+        api_key=os.environ.get("DASHSCOPE_API_KEY"),
+    ),
+    on_disk=False,
+)
